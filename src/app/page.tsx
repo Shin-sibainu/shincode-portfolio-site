75ab57669bcb9ef5f4ac419f538ca08dc8c00209
xxx871 <0kh38c317264.7u@gmail.com>
2024-11-22 17:17:04 +0900

    homeのheroセクション実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Contact from "@/components/layouts/Contact/Contact";
+import HeroSection from "@/components/sections/home/HeroSection";
 
 export default function Home() {
   return (
     <main>
+      <HeroSection />
       <Contact />
     </main>
   );
