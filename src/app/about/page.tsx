151f7fd6a3a04278c94828605ed09103a48661c7
xxx871 <0kh38c317264.7u@gmail.com>
2025-02-15 16:17:19 +0900

    aboutページのmissionセクション作成

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import MissionSection from '@/components/sections/about/MissionSection';
+import React from 'react';
+
+const About = () => {
+  return (
+    <main>
+      <MissionSection />
+    </main>
+  )
+};
+
+export default About;
