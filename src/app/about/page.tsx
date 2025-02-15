00aa57cfc50954c5d1ebbbe394656c4a1eb88e06
xxx871 <0kh38c317264.7u@gmail.com>
2025-02-15 16:24:58 +0900

    aboutページのfeatureセクション実装

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,13 @@
 import MissionSection from '@/components/sections/about/MissionSection';
+import { aboutFeatures } from '@/components/sections/shared/features/featureItems';
+import FeatureSection from '@/components/sections/shared/features/FeatureSection';
 import React from 'react';
 
 const About = () => {
   return (
     <main>
       <MissionSection />
+      <FeatureSection features={aboutFeatures} isAboutPage={true} />
     </main>
   )
 };
